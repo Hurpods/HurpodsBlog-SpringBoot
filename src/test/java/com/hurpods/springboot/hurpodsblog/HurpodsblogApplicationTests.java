ecb1e14849b3c8d5b5161dabb4bd2532c8e0f710
Hurpods <1159992822@qq.com>
2020-05-13 16:43:11 +0800

    初次提交

---
+++ src/test/java/com/hurpods/springboot/hurpodsblog/HurpodsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurpods.springboot.hurpodsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurpodsblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
